028d5920e9e2eb8c45a3a0c6ff0e9101224a000e
MattLBC <matt@Phoenix.localdomain>
2023-01-03 17:05:49 +0000

    Task 4 - Adds MUI cards for each area, minor styling added for this

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { useEffect, useState } from 'react'
-import { getAreaData } from './api'
+import { useEffect, useState } from "react";
+import { getAreaData } from "./api";
+import Card from "@mui/material/Card";
 
-import './App.css'
+import "./App.css";
+import { CardContent } from "@mui/material";
 
 function App() {
   const [areas, setAreas] = useState([]);
-  const [tempPostcode, setTempPostcode] = useState()
+  const [tempPostcode, setTempPostcode] = useState();
   const [postcode, setPostcode] = useState("BB10");
 
   const load = async () => {
     try {
-      const areaData = await getAreaData(postcode)
+      const areaData = await getAreaData(postcode);
       setAreas(areaData);
     } catch (error) {
-      console.log(error)
-      window.alert("todo: fix app")
+      console.log(error);
+      window.alert("todo: fix app");
     }
-  }
+  };
 
   const handlePostcode = (event) => {
     event.preventDefault();
-    setPostcode(tempPostcode)
-  }
+    setPostcode(tempPostcode);
+  };
 
   useEffect(() => {
     load();
@@ -33,13 +35,33 @@ function App() {
       <form onSubmit={handlePostcode}>
         <label>
           Postcode:
-          <input type="text" name="postcode" onChange={(e) => setTempPostcode(e.target.value)}/>
+          <input
+            type="text"
+            name="postcode"
+            onChange={(e) => setTempPostcode(e.target.value)}
+          />
         </label>
         <input type="submit" value="Submit" />
       </form>
       <h2>{`Areas for ${postcode}: ${areas.length}`}</h2>
+      {areas.map((area) => {
+        return (
+          <Card variant="outlined" sx={{ display: 'inline-block', maxWidth: 300 }}>
+            <CardContent>
+              <h3>{area["place name"]}</h3>
+              <div class="flex-container">
+                <p class="flex-child">Latitude {area.latitude}</p>
+                <p class="flex-child">Longitude {area.longitude}</p>
+              </div>
+              <div class="flex-container">
+                <p class="flex-child">{area.state}</p>
+              </div>
+            </CardContent>
+          </Card>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
