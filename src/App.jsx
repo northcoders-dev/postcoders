96b2646654f06491d74bdc8938dc280626110d3d
JJW-27 <josephjonwilliams@outlook.com>
2023-01-03 18:26:58 +0000

    task 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,34 @@ import './App.css';
 
 function App() {
   const [areas, setAreas] = useState([]);
+  const [outcode, setOutcode] = useState('');
+  const [searchComplete, setSearchComplete] = useState(false);
 
-  const load = async () => {
+  const handleInput = e => {
+    setSearchComplete(false)
+    setOutcode(e.target.value);
+  };
+
+  const handleSubmit = async e => {
     try {
-      const areaData = await getAreaData();
+      e.preventDefault();
+      const areaData = await getAreaData(outcode);
       setAreas(areaData);
+      setSearchComplete(true)
     } catch (error) {
       window.alert('todo: fix app');
     }
   };
 
-  useEffect(() => {
-    load();
-  }, []);
-
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Enter beginning of postcode:</label>
+        <input type="text" placeholder="e.g. 'M3'" onChange={handleInput} />
+        <button type="submit">Submit</button>
+      </form>
+      {(searchComplete) && <h2>{`Areas for ${outcode}: ${areas.length}`}</h2>}
     </div>
   );
 }
