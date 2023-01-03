28913f8e3d3979da52021d942b99d659360cab07
MattLBC <matt@Phoenix.localdomain>
2023-01-03 16:31:54 +0000

    Task 3 - Allows users to enter a postcode and displays areas for that input

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import { getAreaData } from './api'
 import './App.css'
 
 function App() {
-
   const [areas, setAreas] = useState([]);
+  const [tempPostcode, setTempPostcode] = useState()
+  const [postcode, setPostcode] = useState("BB10");
 
   const load = async () => {
     try {
-      const areaData = await getAreaData()
+      const areaData = await getAreaData(postcode)
       setAreas(areaData);
     } catch (error) {
       console.log(error)
@@ -17,14 +18,26 @@ function App() {
     }
   }
 
+  const handlePostcode = (event) => {
+    event.preventDefault();
+    setPostcode(tempPostcode)
+  }
+
   useEffect(() => {
     load();
-  }, []);
+  }, [postcode]);
 
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <form onSubmit={handlePostcode}>
+        <label>
+          Postcode:
+          <input type="text" name="postcode" onChange={(e) => setTempPostcode(e.target.value)}/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <h2>{`Areas for ${postcode}: ${areas.length}`}</h2>
     </div>
   )
 }
