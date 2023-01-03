59160a1b2ce160eea37525d4d9dd1c60d9e79889
MattScarthSaunders <m.scarsaund@gmail.com>
2023-01-03 19:34:20 +0000

    minor styling improvements

--- src/api/components/AreaCards.jsx
+++ src/api/components/AreaCards.jsx
@@ -4,25 +4,27 @@ import Typography from "@mui/material/Typography";
 
 const AreaCards = ({ areas }) => {
   return (
-    <ul>
+    <ul className={`AreaCards`}>
       {areas.map((area, index) => {
         return (
-          <Card key={`${+index}`} sx={{ minWidth: 275 }}>
-            <CardContent>
-              <section>
-                <Typography sx={{ fontSize: 14 }}>Place:</Typography>
-                <Typography sx={{ fontSize: 12 }}>
-                  {area["place name"]}, {area.state}
-                </Typography>
-              </section>
-              <section>
-                <Typography sx={{ fontSize: 14 }}>Coordinates:</Typography>
-                <Typography sx={{ fontSize: 12 }}>
-                  {`Longitude: ${area.longitude}, Latitude: ${area.latitude}`}
-                </Typography>
-              </section>
-            </CardContent>
-          </Card>
+          <li key={`${+index}`} className="AreaCard">
+            <Card sx={{ minWidth: 275 }}>
+              <CardContent>
+                <section>
+                  <Typography sx={{ fontSize: 14 }}>Place:</Typography>
+                  <Typography sx={{ fontSize: 12 }}>
+                    {area["place name"]}, {area.state}
+                  </Typography>
+                </section>
+                <section>
+                  <Typography sx={{ fontSize: 14 }}>Coordinates:</Typography>
+                  <Typography sx={{ fontSize: 12 }}>
+                    {`Longitude: ${area.longitude}, Latitude: ${area.latitude}`}
+                  </Typography>
+                </section>
+              </CardContent>
+            </Card>
+          </li>
         );
       })}
     </ul>
