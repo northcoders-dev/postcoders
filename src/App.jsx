c5578644108107bdc32179227db766a475adb519
David <dholmes3031@gmail.com>
2023-01-03 17:33:52 +0000

    completed Task 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,24 @@ import "./App.css";
 
 function App() {
   const [areas, setAreas] = useState([]);
+  const [currentPostcode, setCurrentPostcode] = useState("BB10");
+  const [postcode, setPostcode] = useState("");
 
-  const load = async () => {
+  const load = async (postcode) => {
     try {
-      const areaData = await getAreaData();
+      const areaData = await getAreaData(postcode);
 
       setAreas(areaData);
     } catch (error) {
       window.alert("todo: fix app");
     }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setCurrentPostcode(postcode);
+    await load(postcode);
+    setPostcode("");
   };
 
   useEffect(() => {
@@ -23,7 +32,23 @@ function App() {
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <h2>{`Areas for ${currentPostcode}: ${areas.length}`}</h2>
+      <p>
+        Enter the outcode for the postcode below as follows: e.g. (BB10) for
+        BB10 7ED or (M1) for M1 7ED
+      </p>
+      <form action="submit" onSubmit={handleSubmit}>
+        <label htmlFor="postcodeInput">Enter Outcode:</label>
+        <input
+          id="postcodeInput"
+          type="text"
+          value={postcode}
+          onChange={(e) => {
+            setPostcode(e.target.value);
+          }}
+        />
+        <button>Search for areas</button>
+      </form>
     </div>
   );
 }
