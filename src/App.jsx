590440fea33fd80f4f327ed2f92d169c7b5940d9
Musa Imad Issa <musaissa004@gmail.com>
2023-01-04 13:54:17 +0000

    pass the areaData into the setState function

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ function App() {
 
       areas.concat(areaData);
   
-      setAreas(areas);
+      setAreas(areaData);
     } catch (error) {
       window.alert("todo: fix app")
-      // console.log(error)
+     
     }
   }
 
