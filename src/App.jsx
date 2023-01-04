e14e3f11043e7f01d12e0a4f5472a4e91b072a14
Musa Imad Issa <musaissa004@gmail.com>
2023-01-04 15:45:46 +0000

    change the location state is being declared

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,34 @@
-import { useEffect, useState } from 'react'
-import { getAreaData } from './api'
-import Input from './Input';
-import './App.css'
+import { useEffect, useState } from "react";
+import { getAreaData } from "./api";
+import Input from "./Input";
+import "./App.css";
 
 function App() {
+	const [areas, setAreas] = useState([]);
+	const [postcode, setPostcode] = useState("");
+	const load = async () => {
+		try {
+			const areaData = await getAreaData();
 
-  const [areas, setAreas] = useState([]);
+			areas.concat(areaData);
 
-  const load = async () => {
-    try {
-      const areaData = await getAreaData()
+			setAreas(areaData);
+		} catch (error) {
+			window.alert("todo: fix app");
+		}
+	};
 
-      areas.concat(areaData);
-  
-      setAreas(areaData);
-    } catch (error) {
-      window.alert("todo: fix app")
-     
-    }
-  }
+	useEffect(() => {
+		load();
+	}, []);
 
-  useEffect(() => {
-    load();
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>Postcoders</h1>  
-      <Input/>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>Postcoders</h1>
+			<Input postcode={postcode} setPostcode={setPostcode} />
+			<h2>{`Areas for BB10: ${areas.length}`}</h2>
+		</div>
+	);
 }
 
-export default App
+export default App;
