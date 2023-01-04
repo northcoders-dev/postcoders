7bbead74fe30ea9d33b137ea09786fbbc5ec714f
JJW-27 <josephjonwilliams@outlook.com>
2023-01-04 13:14:33 +0000

    removes unused code and fixes bug where old locations are visible when typing a in new location

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ function App() {
           return { ...currCache, [`${outcode}`]: areaData };
         });
         setSearchComplete(true);
-        console.log(cache);
       } catch (error) {
         window.alert('todo: fix app');
       }
@@ -39,23 +38,29 @@ function App() {
   return (
     <div className="App">
       <h1>Postcoders</h1>
+
       <form onSubmit={handleSubmit}>
         <label>Enter beginning of postcode:</label>
         <input type="text" placeholder="e.g. 'M3'" onChange={handleInput} />
         <button type="submit">Submit</button>
       </form>
-      {searchComplete && <h2>{`Areas for ${outcode}: ${areas.length}`}</h2>}
-      {areas.map(area => {
-        return (
-          <Card key={area['place name']}>
-            <h2>{area['place name']}</h2>
-            <p>Longitude: {area.longitude}</p>
-            <p>Latitude: {area.latitude}</p>
-            <p>Country: {area.state}</p>
-            <p>Country abbreviation: {area['state abbreviation']}</p>
-          </Card>
-        );
-      })}
+
+      {searchComplete && (
+        <main>
+          <h2>{`Areas for ${outcode}: ${areas.length}`}</h2>
+          {areas.map(area => {
+            return (
+              <Card key={area['place name']}>
+                <h2>{area['place name']}</h2>
+                <p>Longitude: {area.longitude}</p>
+                <p>Latitude: {area.latitude}</p>
+                <p>Country: {area.state}</p>
+                <p>Country abbreviation: {area['state abbreviation']}</p>
+              </Card>
+            );
+          })}
+        </main>
+      )}
     </div>
   );
 }
