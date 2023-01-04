b1d5ce4756f75f065ea8f1c865135e44c7c0e559
Carl-with-a-C <carlbclifton@gmail.com>
2023-01-04 08:52:49 +0000

    Created form with input value submitted to state on press of associated button. Postcode is passed to api request and useEffect handles rendering changes.

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,58 @@ import "./App.css";
 
 function App() {
   const [areas, setAreas] = useState([]);
+  const [postcode, setPostcode] = useState("BB10");
+  const [newPostcode, setNewPostcode] = useState({ body: "" });
+  const [errMessage, setErrMessage] = useState("");
 
-  const load = async () => {
+  //api call for areas made here with submitted postcode as argument
+  const load = async (postcode) => {
     try {
-      const areaData = await getAreaData();
+      const areaData = await getAreaData(postcode);
       setAreas([...areaData]);
     } catch (error) {
       window.alert("todo: fix app");
     }
   };
 
+  //on submit postcode (for api) is changed to user submitted posted
+  const submitPostcode = (submittedPostcode) => {
+    setPostcode(submittedPostcode);
+    setNewPostcode({ body: "" });
+  };
+
+  //handles new submissions as postcode is a dependancy
   useEffect(() => {
-    load();
-  }, []);
+    load(postcode);
+  }, [postcode]);
 
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (newPostcode.body) {
+            submitPostcode(newPostcode.body);
+          } else {
+            setErrMessage("Please type postcode area");
+          }
+        }}
+      >
+        <input
+          name="postcode_input"
+          placeholder="postcode"
+          type="text"
+          onChange={(e) =>
+            setNewPostcode({ ...newPostcode, body: e.target.value })
+          }
+          value={newPostcode.body}
+        ></input>
+        <button type="submit">
+          {newPostcode ? "find adress" : "searchIcon"}
+        </button>
+        <h2>{`Areas for ${postcode}: ${areas.length}`}</h2>
+      </form>
     </div>
   );
 }
