7c7011cce61b981e67ff952b2eba82fea7e2d6d9
Carl-with-a-C <carlbclifton@gmail.com>
2023-01-04 16:59:16 +0000

    4. Added card with materialUI. Card has information of retrieved areas and appears on search. mUI added to json. Styling: changed color and background color in index, canged the size of the card and font. Added Postcoders logo. Removed all console.logs

--- COMMENTS.md
+++ COMMENTS.md
@@ -1,1 +1,5 @@
-# put your "I would have done this with more time"-style comments here
+# I would have added more robust verification to the input. At the least a more informative error handling and notification
+
+# I would have liked time to style the card more throroughly
+
+# If I had time to do number 5, in order to create a cache I would have created a state for receivedAreas an object with the newPostcode as a key and the returned areas as an array value. When the user entered a new query I would add conditional logic to the submitPostcode function that would check if the queried code appeared as a key and if so setAreas to the spread of the array value
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { getAreaData } from "./api";
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import pcodersLogo from "./pcodersLogo.svg";
 import "./App.css";
 
 function App() {
@@ -12,17 +15,17 @@ function App() {
   const [errMessage, setErrMessage] = useState("");
   const [postcodeSubmitted, setPostcodeSubmitted] = useState(false);
 
+  //api call for areas made here with submitted postcode as argument
   const load = async (postcode) => {
     try {
-      console.log(postcode, "<<postcode");
       const areaData = await getAreaData(postcode);
       setAreas([...areaData]);
-      console.log(areas, "<areas");
     } catch (error) {
       window.alert({ error });
     }
   };
 
+  //on submit postcode (for api) is changed to user submitted posted
   const submitPostcode = (submittedPostcode) => {
     setPostcode(submittedPostcode);
     setPostcodeSubmitted(true);
@@ -35,7 +38,9 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Postcoders</h1>
+      <img src={pcodersLogo} alt="postcoders logo"></img>
+
+      {/*==========FORM==============*/}
       <form
         className="postcode-input"
         onSubmit={(e) => {
@@ -59,7 +64,32 @@ function App() {
         <button type="submit">Find Areas</button>
       </form>
 
-      <h2>{`Areas for ${postcode}: ${areas.length}`}</h2>
+      {/*==========CARD==============*/}
+      {postcodeSubmitted ? (
+        <Card variant="outlined" sx={{ minWidth: 350 }}>
+          <CardContent>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ fontSize: 18, fontWeight: 500 }}
+            >
+              {`${areas.length} area/s found for ${postcode}`}
+            </Typography>
+            {areas.map((area) => {
+              return (
+                <Typography
+                  key={area["place name"] + area.latitude}
+                  variant="body2"
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  {`${areas.indexOf(area) + 1}. ${area["place name"]}`}
+                </Typography>
+              );
+            })}
+          </CardContent>
+        </Card>
+      ) : null}
     </div>
   );
 }
