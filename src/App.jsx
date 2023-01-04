272a4c82de2663fc1636796a0779fe19ea14996e
Jordan96L <jordan-ling@hotmail.co.uk>
2023-01-04 14:06:15 +0000

    user can input any postcode

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from 'react'
 import { getAreaData } from './api'
-
 import './App.css'
 
 function App() {
 
   const [areas, setAreas] = useState([]);
-
+  const [postcode, setPostcode] = useState('')
+  const [finalPostcode, setFinalPostcode] = useState('')
   const load = async () => {
     try {
-      const areaData = await getAreaData()
+      const areaData = await getAreaData(postcode)
+
   
       setAreas(areaData);
     } catch (error) {
@@ -17,15 +18,35 @@ function App() {
     }
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setFinalPostcode(postcode)
+  try {
+    await getAreaData(finalPostcode)
+  } catch (err) {
+      console.log(err)
+    }
+  }
+
   useEffect(() => {
     load();
-  }, []);
+    setPostcode('')
+  }, [finalPostcode, areas]);
 
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
-    </div>
+      <h2>{`Areas for ${finalPostcode.toUpperCase()}: ${areas.length}`}</h2>
+      <form onSubmit={handleSubmit}>
+        <input 
+        placeholder="Type postcode here..."
+        value={postcode}
+        onChange={(e) => {
+            setPostcode(e.target.value)
+        }}
+        />
+        </form>
+     </div>
   )
 }
 
