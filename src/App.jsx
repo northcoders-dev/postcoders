e1c128501ce3fecdf067d3c6750b5da5fa9cb297
laurie60 <laurie8000@googlemail.com>
2023-01-05 15:50:51 +0000

    fix place count

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,7 @@ function App() {
     try {
       const areaData = await getAreaData();
 
-      areas.concat(areaData);
-
-      setAreas(areas);
+      setAreas(areaData);
     } catch (error) {
       window.alert("todo: fix app");
     }
