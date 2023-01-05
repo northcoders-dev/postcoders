81da6361497640ca25a20fddbcdec3b01f042891
Chloe Jenner <crjdowning@hotmail.com>
2023-01-05 00:17:36 +0000

    Comments added and loading began

--- COMMENTS.md
+++ COMMENTS.md
@@ -1,1 +1,6 @@
 # put your "I would have done this with more time"-style comments here
+1. Look at regex for Task 3. Possibly make it only uppercase, but is this as user friendly? If in a rush, do we all capitalise?
+2. Add loading to be more user friendly.
+3. Error messages for incorrect postcodes.
+4. User friendly CSS options-disabling button if empty or not correct with messages. Less glaring colouring of the screen (consider Irlenes Syndrome).
+5. I have spent some of the time reading on how to cache but not enough time to truly implement with efficiency. However the importance of caching and local storage has been researched.
--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,14 @@ function App() {
   const [areas, setAreas] = useState([]);
   const [value, setValue] = useState("");
   const [newValue, setNewValue] = useState("BB10");
+  const [loading, setLoading] = useState(false)
 
   const load = async () => {
     try {
+      setLoading(true)
       const areaData = await getAreaData(newValue)
       setAreas(areaData);
+      setLoading(false)
     } catch (error) {
       window.alert("todo: fix app")
     }
@@ -23,14 +26,17 @@ function App() {
     load();
   }, [newValue]);
 
-  return (
-    <div className="App">
+  if (loading) {
+    return <p>...Loading ...</p>;
+  }
+
+  
+  return <div className="App">
       <h1>Postcoders</h1>
-      <SearchPostcode value={value} setValue={setValue} setNewValue={setNewValue}/>
+      <SearchPostcode value={value} setValue={setValue} setNewValue={setNewValue} />
       <h2>{`Areas for ${newValue}: ${areas.length}`}</h2>
       <PostcodeResults areas={areas}/>
     </div>
-  )
 }
 
 export default App
