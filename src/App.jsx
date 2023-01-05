af8984b4968257c46376259d6b936d602d341915
alexmbentley <106654240+alexmbentley@users.noreply.github.com>
2023-01-05 17:25:24 +0000

    3. Added form to submit personalised API calls

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,41 @@ import './App.css';
 
 function App() {
   const [areas, setAreas] = useState([]);
+  const [areaCode, setAreaCode] = useState('');
+  const [oldAreaCode, setOldAreaCode] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
-  const load = async () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     try {
-      const areaData = await getAreaData();
-
+      const areaData = await getAreaData(areaCode);
+      setFormSubmitted(true);
+      setOldAreaCode(areaCode);
       setAreas(areaData);
     } catch (error) {
       window.alert(error);
     }
   };
 
-  useEffect(() => {
-    load();
-  }, []);
-
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Please input first half of postcode in here: </label>
+        <input
+          type="text"
+          id="areaCode"
+          value={areaCode}
+          onChange={(e) => setAreaCode(e.target.value.toUpperCase())}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+      {formSubmitted ? (
+        <div>
+          <h2>{`Areas for ${oldAreaCode}: ${areas.length}`}</h2>
+        </div>
+      ) : null}
     </div>
   );
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const getAreaData = async () => {
-  const { data } = await axios.get('https://api.zippopotam.us/GB/bb10');
+export const getAreaData = async (areaCode) => {
+  const { data } = await axios.get(`https://api.zippopotam.us/GB/${areaCode}`);
 
   return data.places;
 };
