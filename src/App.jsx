43b8a7e3dc244cf1e5e01fc65c39ad036149b123
BumbleGoose <i.r.r.laurie@gmail.com>
2023-01-05 11:15:28 +0000

    Add basic form to allow dynamic postcode search. Refactor api/index.js to allow api call to accept dynamic string.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,50 @@
-import { useEffect, useState } from "react";
+import react, { useEffect, useState } from "react";
 import { getAreaData } from "./api";
 
 import "./App.css";
 
 function App() {
   const [areas, setAreas] = useState([]);
+  const [postCodeInput, setPostCodeInput] = useState("");
 
-  const load = async () => {
-    try {
-      const areaData = await getAreaData();
-
-      setAreas(areaData);
-    } catch (error) {
-      window.alert("todo: fix app");
-    }
+  const handlePostCodeInputChange = (event) => {
+    setPostCodeInput(event.target.value);
   };
 
-  useEffect(() => {
-    load();
-  }, []);
+  const handlePostCodeSubmit = (event) => {
+    event.preventDefault();
+    getAreaData(postCodeInput)
+      .then((areaData) => {
+        setAreas(areaData);
+      })
+      .catch((error) => {
+        window.alert("Please provide a valid postcode (outcode only)");
+      });
+  };
 
   return (
-    <div className="App">
-      <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
-    </div>
+    <>
+      <div className="App">
+        <h1>Postcoders</h1>
+        <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      </div>
+
+      <form onSubmit={handlePostCodeSubmit}>
+        <input
+          onChange={handlePostCodeInputChange}
+          type="text"
+          value={postCodeInput}
+          placeholder="Input postcode"
+        />
+        <button
+          id="post__code__submit__btn"
+          type="submit"
+          disabled={postCodeInput === "" ? true : false}
+        >
+          Submit+
+        </button>
+      </form>
+    </>
   );
 }
 
