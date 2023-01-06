e1a0af7d8ce6f1aabe97dd672e97098459c5c16b
Dantren18 <davidarenwick@gmail.com>
2023-01-06 00:41:54 +0000

    3. Users can now search any outcode to receive back the number of areas for that given outcode. Also there is some error handling / verification on the outcode provided to ensure that it is in the correct format, although it only checks the format and not if the outcode actually exists

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,76 @@ import "./App.css";
 
 function App() {
   const [areas, setAreas] = useState([]);
+  const [outcodeSearch, setOutcodeSearch] = useState("");
+  const [placeholderText, setPlaceholderText] = useState(
+    "Type in the postcode you wish to search here"
+  );
+  const [renderedOutcode, setRenderedOutcode] = useState("");
 
   const load = async () => {
     try {
-      const areaData = await getAreaData();
-
+      const areaData = await getAreaData(outcodeSearch);
       setAreas(areaData);
+      setRenderedOutcode(outcodeSearch);
     } catch (error) {
       window.alert("todo: fix app");
     }
   };
 
-  useEffect(() => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let outcodeRegEx = /[A-Z]{1,2}[0-9]{1,2}/i;
+
+    if (outcodeSearch === "") {
+      setPlaceholderText("You must type some text before submitting!");
+      return;
+    }
+
+    if (outcodeSearch.length > 4) {
+      setOutcodeSearch("");
+      setPlaceholderText(
+        "The postcode you entered should have a max of 4 characters."
+      );
+      return;
+    }
+
+    if (!outcodeRegEx.test(outcodeSearch)) {
+      setOutcodeSearch("");
+      setPlaceholderText(
+        "The postcode you entered is in an incorrect format. Please try again."
+      );
+      return;
+    }
     load();
-  }, []);
+  };
 
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <p>
+        Please enter the first part of the postcode you want to search, (e.g.
+        “M1” rather than the full “M1 7ED”)
+      </p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            value={outcodeSearch}
+            placeholder={placeholderText}
+            onChange={(e) => setOutcodeSearch(e.target.value)}
+            className="outcodeEntry"
+          />
+        </label>
+        <br />
+        <input type="submit" value="submit" />
+      </form>
+      {renderedOutcode ? (
+        <h2>
+          {`Areas for ${renderedOutcode.toUpperCase()}: ${areas.length}`}{" "}
+        </h2>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
