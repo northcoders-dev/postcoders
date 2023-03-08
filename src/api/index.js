8f7e6c3214d38a5a460df8637f0b55537a73fd6b
guy0017 <waesin@gmail.com>
2023-03-08 12:22:19 +0000

    removed console logs

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const getAreaData = async (postcode) => { console.log("API CALL.....")
+export const getAreaData = async (postcode) => { 
     const { data } = await axios.get(`https://api.zippopotam.us/GB/${postcode}`);
 
     return data.places;
