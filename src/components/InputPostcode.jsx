af381c1c8abb26af615d52c3c1154016787265c1
guy0017 <waesin@gmail.com>
2023-03-09 16:21:45 +0000

    refactored userinput with regex. Allow only alphanumeric input, locks text field after whitespace is used, unlocks text field when backspace is used

--- src/components/InputPostcode.jsx
+++ src/components/InputPostcode.jsx
@@ -3,18 +3,32 @@ import { useState } from "react";
 export const InputPostcode = ({ setPostcode }) => {
   const [userInput, setUserInput] = useState("");
 
+  let spaceInput = false;
+
   function handleInput(e) {
+    // Regular expressions for: allowing alphanumeric chars only; white space detection
+
+    const regex1 = /\W/g;
+    const regex2 = /\s/g;
+
+    // When white space is detected and text field is locked, the use of backspace will unlock text field
+
+    e.length < userInput.length ? (spaceInput = false) : null;
+
+    // User input restricted to alphanumeric chars
+
+    regex1.test(e) || spaceInput ? null : setUserInput(e);
+
+    // Text field locked when white space is entered, as only outcode is needed
+
+    regex2.test(e) ? spaceInput = true : null;
+  }
+
+  function handleSubmit(e) {
     e.preventDefault();
 
-    let outcode = "";
-
-    userInput.includes(" ")
-      ? outcode = userInput.slice(0, userInput.indexOf(" "))
-      : outcode = userInput;
-    
-    setPostcode(outcode);
+    setPostcode(userInput);
     setUserInput("");
-    
   }
 
   return (
@@ -24,9 +38,11 @@ export const InputPostcode = ({ setPostcode }) => {
         type="text"
         value={userInput}
         placeholder='e.g. "M1" instead of "M1 7ED"'
-        onChange={(e) => setUserInput(e.target.value.toUpperCase())}
+        onChange={(e) => handleInput(e.target.value.toUpperCase())}
       />
-      <button onClick={(e) => handleInput(e)}>Find</button>
+      <button onClick={(e) => handleSubmit(e)}>Find</button>
+      <br />
+      <p>{userInput}</p>
     </form>
   );
 };
